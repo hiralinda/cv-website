a3bbaa93cf7d2c48f5cf1d88cf80fcf7f61773f4
hiralinda <lilirezerata@gmail.com>
2024-08-09 21:14:02 +0200

    fix main href

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <motion.a
-            href="/"
+            href="/hireligia"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}>
             Hire Ligia
